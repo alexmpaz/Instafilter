9b1d091fe7377e97c7edcae8687fff9e88639157
Alex Paz <alexmpaz@gmail.com>
2022-02-25 14:57:30 +0000

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Alex Paz on 25/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
