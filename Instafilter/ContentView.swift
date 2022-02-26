be4757dde6955ad36a080ecd96315386b8ba46a1
Alex Paz <alexmpaz@gmail.com>
2022-02-26 12:18:58 +0000

    feat: onChange modifier demo

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,9 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var blurAmount = 0.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello, Netherlands!")
+                .blur(radius: blurAmount)
+            
+            Slider(value: $blurAmount, in: 0...20)
+            Button("Random blur") {
+                blurAmount = Double.random(in: 0...20)
+            }
+            .onChange(of: blurAmount) { newValue in
+                print("New value is \(newValue)")
+            }
+        }
     }
 }
 
