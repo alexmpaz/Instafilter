500f83e72e5a4362dd6ebf458116f9cad8e740ef
Alex Paz <alexmpaz@gmail.com>
2022-02-26 16:04:54 +0000

    feat: dynamic Image filter properties

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,25 +5,63 @@
 //  Created by Alex Paz on 25/02/2022.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
+    @State private var image: Image?
     
     var body: some View {
-        Text("Hello, Netherlands!")
-            .frame(width: 300, height: 300)
-            .background(backgroundColor)
-            .onTapGesture {
-                showingConfirmation.toggle()
-            }
-            .confirmationDialog("Change Background", isPresented: $showingConfirmation) {
-                Button("Red") { backgroundColor = .red }
-                Button("Green") { backgroundColor = .green }
-                Button("Blue") { backgroundColor = .blue }
-                Button("Cancel", role: .cancel) { }
-            } message: { Text("Select a new color!")}
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage() {
+        guard let inputImage = UIImage(named: "Example") else { return }
+        let beginImage = CIImage(image: inputImage)
+        
+        // context for CIImage and filter
+        let context = CIContext()
+//        let currentFilter = CIFilter.sepiaTone()
+//        let currentFilter = CIFilter.pixellate()
+        let currentFilter = CIFilter.crystallize()
+//        let currentFilter = CIFilter.twirlDistortion()
+        currentFilter.inputImage = beginImage
+//        currentFilter.intensity = 1  // sepia
+//        currentFilter.scale = 50  // pixellate
+//        currentFilter.radius = 50 // crystallize
+//        currentFilter.radius = 800 // twirlDistortion
+//        currentFilter.center = CGPoint(x: inputImage.size.width / 2, y: inputImage.size.height / 2)  // twirlDistortion
+        
+        let amount = 1.0
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
+        }
+        
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey)
+        }
+        
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(amount * 50, forKey: kCIInputScaleKey)
+        }
+        
+        // series of conversions with optionals, using 3
+        
+        // read de CIImage out from our filter, based on provided settings
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
     }
 }
 
